8139ea64e4d693042a7cf3d8e88983b2000f46ae
FangningZhang81 <fangning.zhang@mail.utoronto.ca>
2024-11-24 16:00:54 -0500

    exploratory analysis data uploaded

--- scripts/05-exploratory_data_analysis.R
+++ scripts/05-exploratory_data_analysis.R
@@ -9,29 +9,82 @@
 
 
 #### Workspace setup ####
-library(tidyverse)
-library(rstanarm)
+library(ggplot2)
+library(dplyr)
 
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
+# Load analysis data
+analysis_data <- read_csv("data/02-analysis_data/analysis_data.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# Distribution of current_price by vendor
+ggplot(analysis_data, aes(x = vendor, y = current_price)) +
+  geom_boxplot() +
+  labs(
+    title = "Distribution of Current Price of Potatoes by Vendor",
+    x = "Vendor",
+    y = "Current Price"
+  ) +
+  theme_minimal()
 
+# Distribution of current_price by month
+ggplot(analysis_data, aes(x = as.factor(month), y = current_price)) +
+  geom_boxplot() +
+  labs(
+    title = "Distribution of Current Price of Potatoes by Month",
+    x = "Month",
+    y = "Current Price"
+  ) +
+  theme_minimal()
 
-#### Save model ####
-saveRDS(
-  first_model,
-  file = "models/first_model.rds"
-)
+# Distribution of old_price by vendor
+ggplot(analysis_data, aes(x = vendor, y = old_price)) +
+  geom_boxplot() +
+  labs(
+    title = "Distribution of Old Price of Potatoes by Vendor",
+    x = "Vendor",
+    y = "Old Price"
+  ) +
+  theme_minimal()
 
+# Distribution of old_price by month
+ggplot(analysis_data, aes(x = as.factor(month), y = old_price)) +
+  geom_boxplot() +
+  labs(
+    title = "Distribution of Old Price of Potatoes by Month",
+    x = "Month",
+    y = "Old Price"
+  ) +
+  theme_minimal()
 
+# Total number of products by month
+ggplot(analysis_data, aes(x = as.factor(month))) +
+  geom_bar(fill = "steelblue") +
+  labs(
+    title = "Total Number of Products by Month",
+    x = "Month",
+    y = "Total Products"
+  ) +
+  theme_minimal()
+
+# Total number of products by vendor
+ggplot(analysis_data, aes(x = vendor)) +
+  geom_bar(fill = "grey") +
+  labs(
+    title = "Total Number of Products by Vendor",
+    x = "Vendor",
+    y = "Total Products"
+  ) +
+  theme_minimal()
+
+# Scatterplot of current_price vs. old_price
+analysis_data %>%
+  group_by(old_price, current_price) %>%
+  summarize(count = n()) %>%
+  ggplot(aes(x = old_price, y = current_price, size = count)) +
+  geom_point(alpha = 0.7) +
+  labs(
+    title = "Distribution of Current vs. Old Price of Potatoes (Point Size by Frequency)",
+    x = "Old Price",
+    y = "Current Price",
+    size = "Frequency"
+  ) +
+  theme_minimal()
